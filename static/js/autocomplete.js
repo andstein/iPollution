07324d72161bb7df0c55ce628e3f01e132963a3f
Nicolai Kilian <nicolai.kilian@googlemail.com>
2012-09-28 15:07:25 +0200

    minimized client code, added jquery scripts

--- static/js/autocomplete.js
+++ static/js/autocomplete.js
@@ -1,41 +1,36 @@
-    $(function() {
-        function log( message ) {
-            $( "<div/>" ).text( message ).prependTo( "#log" );
-            $( "#log" ).scrollTop( 0 );
-        }
+$(function() {
+    function log( message ) {
+        $( "#location").text(message);
+        $( "<div/>" ).text( message ).prependTo( "#log" );
+        $( "#log" ).scrollTop( 0 );
+    }
 
-        $( "#location" ).autocomplete({
-            source: function( request, response ) {
-                $.ajax({
-                    url: "http://ws.geonames.org/searchJSON",
-                    dataType: "jsonp",
-                    data: {
-                        featureClass: "P",
-                        style: "full",
-                        maxRows: 12,
-                        name_startsWith: request.term
-                    },
-                    success: function( data ) {
-                        response( $.map( data.geonames, function( item ) {
-                            return {
-                                label: item.name + (item.adminName1 ? ", " + item.adminName1 : "") + ", " + item.countryName,
-                                value: item.name
-                            }
-                        }));
-                    }
-                });
-            },
-            minLength: 2,
-            select: function( event, ui ) {
-                log( ui.item ?
-                    "Selected: " + ui.item.label :
-                    "Nothing selected, input was " + this.value);
-            },
-            open: function() {
-                $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
-            },
-            close: function() {
-                $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
-            }
-        });
+    $( "#location" ).autocomplete({
+        source: function( request, response ) {
+            $.ajax({
+                url: "http://ws.geonames.org/searchJSON",
+                dataType: "jsonp",
+                data: {
+                    featureClass: "P",
+                    style: "full",
+                    maxRows: 12,
+                    name_startsWith: request.term
+                },
+                success: function( data ) {
+                    response( $.map( data.geonames, function( item ) {
+                        return {
+                            value: item.name
+                        }
+                    }));
+                }
+            });
+        },
+        minLength: 2,
+        open: function() {
+            $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+        },
+        close: function() {
+            $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+        }
     });
+});
